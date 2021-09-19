b522b55f9ebc0faf12132b11ad0088be32f2f43c
Cromega <cr.jrg08@gmail.com>
2021-09-19 17:27:28 -0500

    complete table and modal

--- Calculadora_Estadistica_Basica.py
+++ Calculadora_Estadistica_Basica.py
@@ -2,30 +2,60 @@ import numpy as np
 from numpy.core.fromnumeric import size
 from scipy import stats
 import re
+import os
 
+class estadistic():
 
+    def exec(self):
 
-enter_data = input("Ingrese los datos separados por espacios o comas (' ', ','),\n"\
+        enter_data = input("Ingrese los datos separados por espacios o comas (' ', ','),\n"\
                     "los valores decimales señalados por puntos ('.')\n"\
                     "y evite los caracteres alfabeticos diferentes a los indicados antes\n\n"\
                     "Datos: ")
 
+        self.clear_screen()
+        
+        data_pured = data().pure_data(enter_data)
+        mean, mid, mod = data().calculate(data_pured)
+
+        print(f"\nDatos: {data_pured}\n"\
+                f"Tamaño: {data_pured.size} datos\n"
+                f"Media: {mean}\n"\
+                f"Mediana: {mid}\n"\
+                f"Moda: Valor = {np.round(mod.mode[0])} ; Frecuencia = {mod.count[0]}\n")
+
+        self.clear_screen(True)
+
+    def clear_screen(self, wait = False):
+
+        if wait == True:
+
+            os.system("pause")
+        
+        if os.name == "nt":
+
+            os.system("cls")
+        
+        else:
+
+            os.system("clear")
+
 class data():
 
-    def 
+    def pure_data(self, enter_data):
 
-data_pure = re.sub("[a-zA-Z]|,", "", enter_data.strip())
-data_str = re.split(" ", data_pure)
-data = np.array(data_str).astype(np.float_)
-data.sort()
-mean = data.mean()
-indexs = np.array([np.round(data.size/2, 0) - 1, np.round(data.size/2, 0) - 1, np.round(data.size/2, 0)]).astype(np.int_)
-mid = data[indexs[0]] if data.size%2 != 0 else (data[indexs[1]] + data[indexs[2]])/2
+        data_pure = re.sub("[a-zA-Z]|,", "", enter_data.strip())
+        data_str = re.split(" ", data_pure)
+        data = np.array(data_str).astype(np.float_)
+        data.sort()
 
-mod = stats.mode(data)
+        return data
+    
+    def calculate(self, data):
 
-print(f"\nDatos: {data}\n"\
-    f"Tamaño: {data.size} datos\n"
-    f"Media: {mean}\n"\
-    f"Mediana: {mid}\n"\
-    f"Moda: Valor = {np.round(mod.mode[0])} ; Frecuencia = {mod.count[0]}\n")
+        indexs = np.array([np.round(data.size/2, 0) - 1, np.round(data.size/2, 0) - 1, np.round(data.size/2, 0)]).astype(np.int_)
+        mean = data.mean()
+        mid = data[indexs[0]] if data.size%2 != 0 else (data[indexs[1]] + data[indexs[2]])/2
+        mod = stats.mode(data)
+
+        return mean, mid, mod
